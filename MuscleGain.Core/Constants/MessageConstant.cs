32a1c902bb1a5ee4346d7b12fb23eeea681a8e2c
ANGEL <atabakov99@abv.bg>
2022-09-14 02:32:04 +0300

    Add project files.

---
+++ MuscleGain.Core/Constants/MessageConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleGain.Core.Constants
+{
+    public static class MessageConstant
+    {
+        public const string ErrorMessage = "ErrorMessage";
+        public const string SuccessMessage = "SuccessMessage";
+        public const string WarningMessage = "WarningMessage";
+    }
+}
---
+++ MuscleGain/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MuscleGain.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
