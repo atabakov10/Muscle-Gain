0dc9019e156d64c3eafb8ca4ba7aae9edbeecffe
ANGEL <atabakov99@abv.bg>
2022-12-06 04:43:33 +0200

    Fixed some bugs

--- MuscleGain.Core/Services/Statistics/StatisticsService.cs
+++ MuscleGain.Core/Services/Statistics/StatisticsService.cs
@@ -5,25 +5,25 @@ using MuscleGain.Infrastructure.Data;
 
 namespace MuscleGain.Core.Services.Statistics
 {
-    public class StatisticsService : IStatisticsService
-    {
-        private readonly MuscleGainDbContext dbContext;
+	public class StatisticsService : IStatisticsService
+	{
+		private readonly MuscleGainDbContext dbContext;
 
-        public StatisticsService(MuscleGainDbContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }
+		public StatisticsService(MuscleGainDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
 
-        public async Task<StatisticsServiceModel> Total()
-        {
-            var totalProteins = await this.dbContext.Proteins.Where(x=> x.IsDeleted == false && x.OrderId == null).CountAsync();
-            var totalUsers = await this.dbContext.Users.CountAsync();
+		public async Task<StatisticsServiceModel> Total()
+		{
+			var totalProteins = await this.dbContext.Proteins.Where(x => x.IsDeleted == false && x.IsApproved == true && x.OrderId == null).CountAsync();
+			var totalUsers = await this.dbContext.Users.CountAsync();
 
-            return new StatisticsServiceModel
-            {
-                TotalProteins = totalProteins,
-                TotalUsers = totalUsers
-            };
-        }
-    }
+			return new StatisticsServiceModel
+			{
+				TotalProteins = totalProteins,
+				TotalUsers = totalUsers
+			};
+		}
+	}
 }
