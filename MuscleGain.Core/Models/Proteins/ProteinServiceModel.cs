f9220067f26bf7a4cd1455a632f6032fbe06b3fc
ANGEL <atabakov99@abv.bg>
2022-12-16 02:20:03 +0200

    Final changes

---
+++ MuscleGain.Core/Models/Proteins/ProteinServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MuscleGain.Core.Models.Proteins
+{
+    public class ProteinServiceModel
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = null!;
+        public string Grams { get; init; } = null!;
+        public string Flavour { get; init; }= null!;
+        public decimal? Price { get; init; }
+        public string ImageUrl { get; init; }= null!;
+        public string Category { get; init; }= null!;
+    }
+}
